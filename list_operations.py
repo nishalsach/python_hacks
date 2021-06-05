f02bc94cd3af48756a68dd8f326c118874eb6102
Sachita Nishal <sachita.nishal@gmail.com>
2021-06-04 22:52:45 -0500

    Delete list_operations.py

--- list_operations.py
+++
@@ -1,3 +0,0 @@
-# Flatten list of lists - https://stackoverflow.com/questions/11264684/flatten-list-of-lists
-list_of_lists = [[180.0], [173.8], [164.2], [156.5], [147.2], [138.2]]
-flattened = [val for sublist in list_of_lists for val in sublist]
